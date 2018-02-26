5a73f88cb9d1162a5e0145c0bcddd2b51bef0166
Atticus Zambrana <atticus.zambrana@angusbeefgaming.net>
2018-02-26 10:06:29 -0800

    Added Import statements and package

--- bungee/src/main/java/net/angusbeefgaming/sc/StaffChat.java
+++ bungee/src/main/java/net/angusbeefgaming/sc/StaffChat.java
@@ -1,5 +1,9 @@
+package net.angusbeefgaming.sc;
+
 import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.plugin.Plugin;
+
+import net.angusbeefgaming.sc.command.*;
 
 public class StaffChat extends Plugin {
     public void onEnable() {
